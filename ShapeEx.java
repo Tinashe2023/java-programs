e639748cf64f651224d5b11d17f7d971b2149c78
Tinashe Hando <tinashehando@gmail.com>
2026-03-26 12:51:35 +0530

    New programs

--- ShapeEx.java
+++ ShapeEx.java
@@ -29,10 +29,18 @@ public class ShapeEx{
         Circle c1 = new Circle();
         Rectangle r1 = new Rectangle();
         Triangle t1 = new Triangle();
+
+        System.out.println("\nCircle");
         s1 = c1;
         s1.draw();
+        s1.show();
+
+        System.out.println("\nRectangle");
         s1 = r1;
         s1.draw();
+        s1.show();
+
+        System.out.println("\nTriangle");
         s1 = t1;
         s1.draw();
         s1.show();
